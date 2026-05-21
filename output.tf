5e3a5be957eb7c19c06cd2af36dc43d16823147d
Rajiv Shakya <rajivshaky@gmail.com>
2026-05-21 16:58:29 +0530

    redeploy infra2

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
-output "codecommit_clone_url_http" {
+output "clone_url_http" {
 
   value = module.codecommit.repository_clone_url_http
 
 }
 
-output "codecommit_repository_name" {
+output "repository_name" {
 
   value = module.codecommit.repository_name
 
